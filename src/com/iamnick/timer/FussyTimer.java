3dadba859a9fecdc82f132ffc30897b1ccc713b1
1amnick <nic1115v@gmail.com>
2017-02-22 17:06:02 -0500

    updated version/about

--- src/com/iamnick/timer/FussyTimer.java
+++ src/com/iamnick/timer/FussyTimer.java
@@ -16,13 +16,14 @@ public class FussyTimer {
 	
 	//remember to change these numbers incrementally Kappa
 	
-	static double versionNumber = 1.04;
+	static double versionNumber = 1.05;
 	//make sure you change the version.txt file to fit and comply with FDC and HBP standards.
-	static String version = "FussyTimer v"+versionNumber+" 20.2.17 The \"have you tried turning it off and on again?\" Edition. More info at https://git.io/vDFwj";
+	static String version = "FussyTimer v"+versionNumber+" 22.2.17 The \"I, eat, commas, for, breakfast,\" Edition. More info at https://git.io/vDFwj";
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
-		
+	
+		version.replaceAll(".", ",");
 		PrintStream out = new PrintStream(new FileOutputStream("log.log"));
 		System.setOut(out);
 		System.setErr(out);
